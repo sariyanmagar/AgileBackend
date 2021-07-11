20b86aa13bd628da98c785d4fb948389036012c6
Sariyan <Sariyan@gmail.com>
2021-07-11 10:45:04 +0545

    authentication for admin changed

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,11 +22,19 @@ module.exports.verifyUser=function(req,res,next){
 }
 
 module.exports.verifyAdmin=function(req,res,next){
-    if(!req.user){
-        return res.status(401).json({message:"Unuthorized(Login failed)!!"})
+    try{
+        const token=req.headers.authorization.split(" ")[1];
+        const data= jwt.verify(token, ' anysecretkey');
+        this.verifyAdmin.findOne({_id:data.userId})
+        .then(function(userData){
+            req.user=userData;
+            next()
+        })
+        .catch(function(err){
+            res.status(401).json({error:err});
+        })
     }
-    else if(req.user.role!=="Admin"){
-        return res.status(401).json({message:"Unauthorized( Not an Admin!"})
+    catch(err){
+        res.status(401).json({error:err})
     }
-    next();
 }
