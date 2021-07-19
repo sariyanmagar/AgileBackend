0dc92148bff4c24fcede939cc80e6b9b2c83b7db
Sariyan <Sariyan@gmail.com>
2021-07-19 15:23:40 +0545

    Delete product from cart completed

--- controllers/rentCartController.js
+++ controllers/rentCartController.js
@@ -3,8 +3,8 @@ const auth=require('../middleware/auth');
 const RentCart = require('../models/rentCartModel');
 
 
-//............................SHOW PRODUCT IN CART..............................
-exports.get=(req,res)=>{
+//............................SHOW PRODUCT IN CART.................................................................
+exports.get_product_for_cart=(req,res)=>{
     RentCart.find({user:req.user._id}).populate("product").exec(function(err,rentCarts){
         if(err){
             return res.status(500).json({
@@ -20,7 +20,7 @@ exports.get=(req,res)=>{
     })
 }
 
-//.....................ADD PRODUCT TO CART..............................
+//.............................ADD PRODUCT TO CART....................................................................
 exports.add_to_rentCart=(req,res)=>{
     var data={
         product:req.body.productId,
@@ -58,4 +58,21 @@ exports.add_to_rentCart=(req,res)=>{
             })
         }
     }
+}
+
+//...................................DELETE FROM RENT CART.........................................................
+exports.delete_from_cart=(req,res)=>{
+    rentCart.findOneAndDelete({user:req.user._id,product:req.params.id},function(err,rentCarts){
+        if(err){
+            return res.status(500).json({
+                success:false,
+                message:err.message
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            message:"Item removed from the cart!!",
+            data:rentCarts
+        })
+    })
 }
--- routes/rentCartRoute.js
+++ routes/rentCartRoute.js
@@ -1,4 +1,11 @@
 const express=require('express');
 const router=express.Router();
 
+const RentCartController=require('../controllers/rentCartController');
+
+router.get('/get/rentcart', RentCartController.add_to_rentCart);
+
+router.post('/add/to/rentcart', RentCartController.add_to_rentCart);
+
+
 module.exports=router;
