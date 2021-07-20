3be889b2ea957677c0fe64476751aa67d5b1761d
Sariyan <Sariyan@gmail.com>
2021-07-20 14:00:30 +0545

    Add to cart completed

--- routes/rentCartRoute.js
+++ routes/rentCartRoute.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const router=express.Router();
+const auth=require('../middleware/auth');
+
 
 const RentCartController=require('../controllers/rentCartController');
 
-router.get('/get/rentcart', RentCartController.add_to_rentCart);
+router.get('/get/rentcart',auth.verifyUser,RentCartController.add_to_rentCart);
 
-router.post('/add/to/rentcart', RentCartController.add_to_rentCart);
+router.post('/add/to/rentcart',auth.verifyUser, RentCartController.add_to_rentCart);
+router.delete('/delete/rentcart',auth.verifyUser, RentCartController.delete_from_cart);
 
 
 module.exports=router;
