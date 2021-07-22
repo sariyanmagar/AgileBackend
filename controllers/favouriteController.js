580f03a77020c422f2776cb26c9917433171a1db
Sariyan <Sariyan@gmail.com>
2021-07-22 16:18:45 +0545

    add to favourite

--- controllers/favouriteController.js
+++ controllers/favouriteController.js
@@ -1,5 +1,7 @@
 const Favourite=require('../models/favouritesModel');
 
+
+//............................GET FAVOURITE PRODUCT......................
 exports.get_favourites=(req,res)=>{
     Favourite.find({user:req.user._id}).populate("product").exec(function(err,favourite){
         if(err){
@@ -16,6 +18,7 @@ exports.get_favourites=(req,res)=>{
     })
 }
 
+//................................DELETE FAVOURITE PRODUCT................................
 exports.delete_favourites=(req,res)=>{
     Favourite.findOneAndDelete({user:req.user._id, product:req.params.id},function(err,favourite){
         if(err){
@@ -29,5 +32,42 @@ exports.delete_favourites=(req,res)=>{
             message:"Item removed from the favourite!!",
             data:favourite
         })
+    })
+}
+
+//............................ADD TO FAVOURITES................................
+exports.add_to_favourite=(req,res)=>{
+    var data={
+        product:req.body.productId,
+        user:req.user._id,
+    }
+    Favourite.findOne({user:req.user._id, product:req.body.productId},function(err, favourite){
+        if(favourite){
+            Favourite.findOneAndUpdate({_id : favourite._id}).then(function(favourite){
+                return res.status(200).json({
+                    success : true,
+                    message : "Item Added to favourite Successfully!!",
+                    favourite : favourite
+                })
+            }).catch(err => {
+                return res.status(500).json({
+                    succeess : false,
+                    messsage : err.message
+                })
+            })
+        }else{
+            Favourite.create(data).then(function(favourite){
+                return res.status(200).json({
+                    success : true,
+                    message : "Item Added to favourite Successfully!!",
+                    favourite : favourite
+                })
+            }).catch(err => {
+                return res.status(500).json({
+                    succeess : false,
+                    messsage : err.message
+                })
+            })
+        }
     })
 }
