0cbead067cbb3b408e24ceb79bdffe7aadc98726
Sariyan <Sariyan@gmail.com>
2021-07-28 11:00:20 +0545

    order controller and model modified

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,18 +1,6 @@
 const Order=require('../models/orderModel');
 
-//...................GET ORDER...............................
-exports.get_order=(req,res)=>{
-    Order.find({user:req.user._id}).populate("product").exec(function(err, order){
-        if(err){
-            return res.status(500).json({
-                success:false,
-                message:err.message
-            })
-        }
-        return res.status(200).json({
-            success:true,
-            message:"Order items",
-            data:order
-        })
-    })
+exports.add_order=(req,res)=>{
+    const product=req.body.product;
+    
 }
--- models/orderModel.js
+++ models/orderModel.js
@@ -1,11 +1,12 @@
 const mongoose=require('mongoose');
 
 const Order=mongoose.model('Order',{
-    product:{
+    allproduct:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Product',
         require:true
     },
+],
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
