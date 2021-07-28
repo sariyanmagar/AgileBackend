1ad02d2f8cd841e156dbe86f2d4cacb45947eafc
Sariyan <Sariyan@gmail.com>
2021-07-28 11:04:05 +0545

    model created for promocode

---
+++ models/promocodeModel.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+
+const Promocode=mongoose.model('Promocode',{
+    code:{
+        type:String
+    },
+    percent:{
+        type:Number,
+    },
+    active:{
+        type:String,
+        enum:["Yes", "No"]
+    }
+})
+module.exports=Promocode
