520fef7ef820e55e9b374828372f224204b5fb6d
Sariyan <Sariyan@gmail.com>
2021-08-01 13:48:14 +0545

    rent bill model modified

--- models/rentBillModel.js
+++ models/rentBillModel.js
@@ -2,11 +2,11 @@ const mongoose=require('mongoose');
 const {modelName}=require('./productModel')
 
 const RentBill=mongoose.model('RentBill',{
-    product:{
+    products:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Product',
         require:true
-    },
+    }],
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
@@ -18,15 +18,23 @@ const RentBill=mongoose.model('RentBill',{
     },
     rent_date:{
         type:Date,
+        default: Date.now()
+    },
+    return_date:{
+        type:Date,
     },
     days_rented:{
         type:Number,
     },
-    calc_price:{
-        type:Number,
-    },
     total_price:{
         type:Number,
+    },
+    due_remaining:{
+        type:Number,
+    },
+    returned:{
+        type:Boolean,
+        default:false
     }
 })
 
