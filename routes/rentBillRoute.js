dd98a47ac1f482200188df43ac876890cb99b710
Sariyan <Sariyan@gmail.com>
2021-08-01 13:49:25 +0545

    delete and update added in rent bill route

--- routes/rentBillRoute.js
+++ routes/rentBillRoute.js
@@ -5,9 +5,10 @@ const auth=require('../middleware/auth')
 
 const RentBillController=require('../controllers/rentBillController');
 
-router.get('/get/rentbill',auth.verifyUser,rentBillController.get_rentbill);
-router.post('/add/rentbill', auth.verifyUser,rentBillController.add_to_rentbill);
-router.delete('/delete/rentbill/:id',auth.verifyUser,rentBillController.delete_rentbill);
+router.get('/get/rentbill',auth.verifyUser,RentBillController.get_rentbill);
+router.post('/add/rentbill', auth.verifyUser,RentBillController.add_to_rentbill);
+router.delete('/delete/rentbill/:id',auth.verifyUser,RentBillController.delete_rentbill);
+router.put('/update/rentbill/:id',auth.verifyUser,RentBillController.update_rentbill);
 
 
 module.exports=router;
