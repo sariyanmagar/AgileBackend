a2026ceca20c56632f379f784c3bcfb5a9083436
Sariyan <Sariyan@gmail.com>
2021-08-16 13:13:41 +0545

    error handled

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -12,7 +12,7 @@ router.put('/update/product/:id',fileUpload.single("image"),ProductController.up
 
 router.delete('/deleteproduct/:id', ProductController.delete_product);
 
-router.get('/getallproducts/', ProductController.get_all_products);
+router.get('/getallproducts/:sortType', ProductController.get_all_products);
 
 router.get('/getsingleproduct/:id',ProductController.get_single_product);
 
