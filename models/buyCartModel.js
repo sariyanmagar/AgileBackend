28ad09c8b7729a8b15e093740420cb78f4ecdf9c
Sariyan <Sariyan@gmail.com>
2021-08-21 12:11:08 +0545

    buycart model modified

--- models/buyCartModel.js
+++ models/buyCartModel.js
@@ -2,21 +2,50 @@ const mongoose=require('mongoose');
 const {modelName}=require('./productModel')
 
 const BuyCart=mongoose.model('BuyCart',{
-    product:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Product',
-        require:true
-    },
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
         require:true
     },
-    quantity:{
-        type:Number,
-        require:true,
-        default:1
+    order:[{
+        product:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Product',
+            require:true
+        },
+        quantity:{
+            type:Number,
+            require:true,
+            default:1
+        },
+    }],
+    grandTotal:{
+        type:Number
+    },
+    status:{
+        type:String,
+        emun:["default","pending","delivered"],
+        default:"default"
     }
+    // product:{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:'Product',
+    //     require:true
+    // },
+    // user:{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:'User',
+    //     require:true
+    // },
+    // quantity:{
+    //     type:Number,
+    //     require:true,
+    //     default:1
+    // },
 })
 
 module.exports=BuyCart;
+
+
+
+
