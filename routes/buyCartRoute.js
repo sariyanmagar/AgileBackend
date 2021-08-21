97a9e991e94a9c120c3f0033460f42ebc313b05e
Sariyan <Sariyan@gmail.com>
2021-08-21 12:09:54 +0545

    changes in route of buycart

--- routes/buyCartRoute.js
+++ routes/buyCartRoute.js
@@ -5,7 +5,11 @@ const auth = require("../middleware/auth")
 const BuyCartController=require('../controllers/buyCartController');
 
 router.get('/get/buycart', auth.verifyUser, BuyCartController.get_buycart);
+router.get('/get/trending', BuyCartController.get_trending);
+router.get('/get/default_buycart', auth.verifyUser, BuyCartController.get_default_buycart);
 router.post('/add/buycart',auth.verifyUser, BuyCartController.add_to_buycart);
+router.put('/update/default_to_pending',auth.verifyUser, BuyCartController.update_buycart_to_pending);
+router.put('/update/pending_to_delivered',auth.verifyUser, BuyCartController.update_buycart_to_delivered);
 router.delete('/delete/buycart/:id',auth.verifyUser,BuyCartController.delete_buycart);
 
 module.exports=router;
