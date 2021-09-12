4838e00eb16b209021049072fea7e2f9996c1eb2
Sariyan <Sariyan@gmail.com>
2021-09-12 11:23:23 +0545

    error handled

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -7,6 +7,5 @@ const auth=require('../middleware/auth');
 router.post('/add/comment',auth.verifyUser, commentController.addComments);
 router.post('/get/comments',auth.verifyUser,commentController.getCommentByProduct);
 router.delete('delete/comments',auth.verifyUser,commentController.destroyComment);
-router.post('/addReply',auth.verifyUser,commentController.addReply);
 
 module.exports=router
